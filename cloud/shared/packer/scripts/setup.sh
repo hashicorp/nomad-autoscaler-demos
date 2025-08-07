9dff3b10241d7e9d36430fb373a0460e79e90894
Juana De La Cuesta <juanita.delacuestamorales@hashicorp.com>
2025-08-07 17:12:13 +0200

    Update setup.sh

--- cloud/shared/packer/scripts/setup.sh
+++ cloud/shared/packer/scripts/setup.sh
@@ -43,7 +43,7 @@ sudo ufw disable || echo "ufw not installed"
 curl -sL -o consul.zip ${CONSULDOWNLOAD}
 
 ## Install
-sudo unzip -o consul.zip -o -d /usr/local/bin
+sudo unzip -o consul.zip -d /usr/local/bin
 
 sudo chmod 0755 /usr/local/bin/consul
 sudo chown root:root /usr/local/bin/consul

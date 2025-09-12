9fce1446a6d963342716135e4370d6bc79d4e3e8
Mikael Sikora <89491078+MikaelHashiCorp@users.noreply.github.com>
2025-09-12 16:06:43 -0700

    Updated packer server config

--- cloud/shared/packer/scripts/server.sh
+++ cloud/shared/packer/scripts/server.sh
@@ -78,10 +78,33 @@ echo "nameserver $DOCKER_BRIDGE_IP_ADDRESS" | sudo tee /etc/resolv.conf.new
 cat /etc/resolv.conf | sudo tee --append /etc/resolv.conf.new
 sudo mv /etc/resolv.conf.new /etc/resolv.conf
 
+# Set /opt permissions
+sudo find /opt -type d -exec chmod g+s {} \\;
+sudo chown -R root:ubuntu /opt
+sudo chmod -R g+rX /opt
 
 # Set env vars for tool CLIs
 echo "export CONSUL_RPC_ADDR=$IP_ADDRESS:8400" | sudo tee --append /home/$HOME_DIR/.bashrc
 echo "export CONSUL_HTTP_ADDR=$IP_ADDRESS:8500" | sudo tee --append /home/$HOME_DIR/.bashrc
 echo "export NOMAD_ADDR=http://$IP_ADDRESS:4646" | sudo tee --append /home/$HOME_DIR/.bashrc
 echo "export JAVA_HOME=/usr/lib/jvm/java-8-openjdk-amd64/jre"  | sudo tee --append /home/$HOME_DIR/.bashrc
+echo "alias env='env -0 | tr "\0" "\n" | sort'" | sudo tee --append /home/$HOME_DIR/.bashrc
 
+# set terminal color
+echo "export TERM=xterm-256color" | sudo tee --append /home/$HOME_DIR/.bashrc
+# set terminal prompt
+local marker="# >>> custom WarpTerminal PS1 block >>>"
+if ! grep -Fq "$marker" ~/.bashrc; then
+  cat <<'EOF' >> ~/.bashrc
+# >>> custom WarpTerminal PS1 block >>>
+if [[ $TERM_PROGRAM == "WarpTerminal" ]]; then
+  PS1="\[\033[0;33m\](\$PROMPTID)[Int: \$PRIIP / Ext: \$PUBIP] \[\033[01;32m\]\u\[\033[00m\]:\[\033[01;34m\]\w\[\033[00m\]\$ "
+else
+  PS1="\[\033[0;33m\](\$PROMPTID)[Int: \$PRIIP / Ext: \$PUBIP]\[\033[0m\]\n\[\033[01;32m\]\u\[\033[00m\]:\[\033[01;34m\]\w\[\033[00m\]\$ "
+fi
+# <<< custom WarpTerminal PS1 block <<<
+EOF
+  echo "[INFO] Appended PS1 block to .bashrc."
+else
+  echo "[INFO] PS1 block already in .bashrc."
+fi
